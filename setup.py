22f23544748f603f187caaeb397b874b4c161b94
Alex Sandro <geniofuturo@gmail.com>
2015-09-02 16:26:58 -0300

    add setup script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='xlscsv',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/alexsilva/xlsCsv',
+    license='MIT',
+    author='alex',
+    author_email='',
+    description='Write similar xlsx files to csv format.'
+)
