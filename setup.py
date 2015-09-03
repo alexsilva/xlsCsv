19ad0c3d686d84100558095ad1ab53be9f036ce3
Alex Sandro <geniofuturo@gmail.com>
2015-09-03 12:31:33 -0300

    Try install requires.

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from distutils.core import setup
 
-setup(
-    name='xlscsv',
-    version='1.0',
-    packages=[''],
-    url='https://github.com/alexsilva/xlsCsv',
-    license='MIT',
-    author='alex',
-    author_email='',
-    description='Write similar xlsx files to csv format.'
-)
+with open('requirements.txt') as f:
+    requires = f.read().splitlines()
+    setup(
+        name='xlscsv',
+        version='1.0',
+        packages=[''],
+        url='https://github.com/alexsilva/xlsCsv',
+        license='MIT',
+        author='alex',
+        author_email='',
+        description='Write similar xlsx files to csv format.',
+        install_requires=requires
+    )
